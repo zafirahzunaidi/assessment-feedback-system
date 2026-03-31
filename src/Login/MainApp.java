97246b9382f1c7a899507175853c16800ab82b77
Siti Nur Zafirah Zunaidi <snzfirahhz@gmail.com>
2026-04-01 04:53:53 +0800

    Add files via upload

---
+++ src/Login/MainApp.java
@@ -0,0 +1,11 @@
+package Login;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new AFS_LoginPage().setVisible(true);
+        });
+    }
+}
+
